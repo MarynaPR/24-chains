16bc8e8bdec824c91a2307f28b067a190606cb39
Duncan <duncanmarten@yahoo.com>
2020-12-17 13:26:43 -0600

    added favorite api call and fixed login and signup to call correct ids

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,24 +1,31 @@
 const signupFormHandler = async function (event) {
     event.preventDefault();
 
-    const firstNameEl = document.querySelector("#username-input-signup");
-    const lastNameEl = document.querySelector("#username-input-signup");
+    const firstNameEl = document.querySelector("#firstname-input-signup");
+    const lastNameEl = document.querySelector("#lastname-input-signup");
     const usernameEl = document.querySelector("#username-input-signup");
     const passwordEl = document.querySelector("#password-input-signup");
-    fetch("/api/user", {
-        method: "post",
-        body: JSON.stringify({
-            firstname: firstNameEl.value,
-            lastname: lastNameEl.value,
-            username: usernameEl.value,
-            password: passwordEl.value
-        }),
-        headers: { "Content-Type": "application/json" }
-    })
-        .then(function () {
-            document.location.replace("/dashboard");
+    const emailEl = document.querySelector("#email-input-signup");
+
+    if (usernameEl && passwordEl && emailEl) {
+        const response = await fetch("/api/user", {
+            method: "post",
+            body: JSON.stringify({
+                firstname: firstNameEl.value,
+                lastname: lastNameEl.value,
+                username: usernameEl.value,
+                password: passwordEl.value,
+                email: emailEl.value
+            }),
+            headers: { "Content-Type": "application/json" }
         })
-        .catch(err => console.log(err));
+            
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText)
+        }
+    }
 };
 
 document
