a5918767f52f0d463086100ac642f2ec30cbae1e
frostyausty <priceaustin62@gmail.com>
2020-12-16 20:27:03 -0600

    added post new review js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,8 +4,10 @@ const { Review, User, Course, Rate } = require('../../models');
 router.post('/', (req, res) => {
     Review.create({ 
         course_id: req.body.course_id,
-        user_id: req.body.user_id,
-        review_content: req.body.review_content
+        review_content: req.body.review_content,
+        rating: req.body.rating,
+        user_id: req.body.user_id
+        
      })
      .then(dbReview => res.json(dbReview))
      .catch(err => {
--- public/javascript/review.js
+++ public/javascript/review.js
@@ -1,7 +1,26 @@
 const newReview = async function(event) {
     event.preventDefault();
 
-    const 
+    const course_id = document.querySelector('input[name="course-id"]').value;
+    const review_content = document.querySelector('textarea[name="review-body"]').value;
+    const rating = document.querySelector('input[name="course-rating"]').value;
+    const user_id = document.querySelector(req.session.user_id).value;
 
-}
+    await fetch(`api/post`, {
+        method: "POST",
+        body: JSON.stringify({
+            course_id,
+            review_content,
+            rating,
+            user_id
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+
+    document.location.reload();
+};
+
+document.querySelector("#new-review").addEventListener("submit", newReview);
 
