b471c376ce5414d6f6e58f905fc1a25d0132e762
In Code We Trust <njderenne@gmail.com>
2020-12-20 19:45:34 -0600

    displays courses after clicking the search button from any page

--- public/javascript/course-search.js
+++ public/javascript/course-search.js
@@ -1,18 +1,8 @@
 async function searchClickHandler(event) {
     event.preventDefault();
 
-    const response = await fetch('/api/course/', {
-        method: 'GET',            
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        document.location.replace('/courses');
-    } else {
-        alert(response.statusText)
-    }
+    document.location.replace('/courses');
+    
 }
 
 document.querySelector('#search-course').addEventListener('click', searchClickHandler);
