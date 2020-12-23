f3dbdce620eff471489d1ef0c5414b8a36680e27
frostyausty <priceaustin62@gmail.com>
2020-12-23 14:45:11 -0600

    commented out course search broken code

--- public/javascript/course-search.js
+++ public/javascript/course-search.js
@@ -1,16 +1,16 @@
 async function searchClickHandler(event) {
     event.preventDefault();
 
-    const searchRequest = document.querySelector('input[name="search-input"]').value;
+    // const searchRequest = document.querySelector('input[name="search-input"]').value;
 
-    if(searchRequest) {
-        const response = await fetch('../api/course', {
-            method: "GET",
-            body: JSON.stringify({
+    // if(searchRequest) {
+    //     const response = await fetch('../api/course', {
+    //         method: "GET",
+    //         body: JSON.stringify({
                 
-            })
-        })
-    }
+    //         })
+    //     })
+    // }
 
     //SELECT * FROM course WHERE course_name LIKE '%water%';
     document.location.replace('/courses');
