66a957a297836b84af383c0016846bbefcf0cc05
Duncan <duncanmarten@yahoo.com>
2021-01-03 22:09:40 -0600

    created a way to search by city

--- public/javascript/course-search.js
+++ public/javascript/course-search.js
@@ -1,19 +1,22 @@
 async function searchClickHandler(event) {
     event.preventDefault();
 
-    // const searchRequest = document.querySelector('input[name="search-input"]').value;
+    const searchRequest = document.querySelector('input[name="search-input"]').value;
 
-    // if(searchRequest) {
-    //     const response = await fetch('../api/course', {
-    //         method: "GET",
-    //         body: JSON.stringify({
+    if(searchRequest) {
+        const response = await fetch(`../api/course/city/${searchRequest}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        })
 
-    //         })
-    //     })
-    // }
+        if(response.ok) {
+            document.location.replace(`/courses/${searchRequest}`);
+        } else {
+            alert(response.statusText);
+        }
+    }
 
     //SELECT * FROM course WHERE course_name LIKE '%water%';
-    document.location.replace('/courses');
 
 }
 
